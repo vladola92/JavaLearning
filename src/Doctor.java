4ad0c27894d7537affbd5f9f72fc9e738eca43c6
Ola <vladola92@github.com>
2020-06-18 21:52:24 +0300

    putin structurat

--- src/Doctor.java
+++ src/Doctor.java
@@ -4,12 +4,43 @@ public class Doctor {
     float weight;
     String sections;
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public void working (){
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return this.weight;
+    }
+
+    public void setSections(String sections) {
+        this.sections = sections;
+    }
+
+    public String getSections() {
+        return this.sections;
+    }
+
+    public void working() {
         System.out.println("The doctor is working hard");
     }
 
-    public void eating (){
+    public void eating() {
         System.out.println("The doctor is taking the lunch break");
     }
 }
