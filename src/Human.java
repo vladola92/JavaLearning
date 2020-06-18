aa5999b39209d184a0087d68eb1826f41c802f36
Ola <vladola92@github.com>
2020-06-18 22:36:24 +0300

    putin structurat

--- src/Human.java
+++ src/Human.java
@@ -6,10 +6,51 @@ public class Human {
     int height;
     String name;
 
-    public void working (){
-        System.out.println( name+ " is working hard ");
+    public void setSex(String sex) {
+        this.sex = sex;
     }
-    public void joging (){
+
+    public String getSex() {
+        return this.sex;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setWeightl(int weightl) {
+        this.weightl = weightl;
+    }
+
+    public int getWeightl() {
+        return this.weightl;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void working() {
+        System.out.println(name + " is working hard ");
+    }
+
+    public void joging() {
         System.out.println(name + "is running ");
     }
 }
